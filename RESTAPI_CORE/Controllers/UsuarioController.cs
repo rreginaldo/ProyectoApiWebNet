97c226c00d424d79323920af9c6576d97c08c5da
rreginaldo <diegoroberto2704@gmail.com>
2024-03-05 10:37:04 -0500

    Se agrega validacion login

---
+++ RESTAPI_CORE/Controllers/UsuarioController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using RESTAPI_CORE.Modelos;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace RESTAPI_CORE.Controllers
+{
+    [EnableCors("ReglasCors")]
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class UsuarioController : ControllerBase
+    {
+        private readonly string cadenaSQL;
+        public UsuarioController(IConfiguration config)
+        {
+            cadenaSQL = config.GetConnectionString("CadenaSQL");
+        }
+
+        //REFERENCIAS
+        //MODELO
+        //SQL
+
+        [HttpGet]
+        [Route("LoginUsuario")]
+        public IActionResult LoginUsuario(string correo, string clave)
+        {
+            List<Usuario> lista = new List<Usuario>();
+
+            try
+            {
+                using (var conexion = new SqlConnection(cadenaSQL))
+                {
+                    conexion.Open();
+                    var cmd = new SqlCommand("usp_LoginUsuario", conexion);
+                    cmd.Parameters.AddWithValue("correo", correo);
+                    cmd.Parameters.AddWithValue("clave", clave);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (var rd = cmd.ExecuteReader())
+                    {
+                        while (rd.Read())
+                        {
+                            lista.Add(new Usuario
+                            {
+
+                                id = Convert.ToInt32(rd["id"].ToString()),
+                                nombre = rd["nombre"].ToString(),
+                                rol = rd["rol"].ToString()
+
+                            });
+                        }
+                    }
+                }
+
+                var response = new Response<List<Usuario>>(ResponseType.Success, lista);
+                return StatusCode(StatusCodes.Status200OK, response);
+            }
+            catch (Exception ex)
+            {
+                var response = new Response<List<Usuario>>(ResponseType.Error, ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, response);
+            }
+        }
+    }
+}
---
+++ RESTAPI_CORE/Modelos/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace RESTAPI_CORE.Modelos
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string rol { get; set; }
+    
+       
+    }
+}
